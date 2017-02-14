91147fba374cb7b040816c6eef1252fea5937f8b
Gabriel Age <0800.grc@gmail.com>
2017-02-14 11:22:46 -0200

    Adding better comments and more flexible token creation @ business

--- src/business/token.business.js
+++ src/business/token.business.js
@@ -18,7 +18,18 @@ class TokenBusiness {
         return Token.findOneAndUpdate({_id: tokenvm._id}, tokenvm);
     };
 
+    /*
+    * tokenvm could be either a complete token json or a single user Id
+    */
     create(tokenvm) {
+        if(typeof(params) != 'object') {
+            let today = new Date().getTime();            
+            tokenvm = {
+                user_id: tokenvm,
+                expire: new Date(today + 36000000)
+            };
+        }
+
         return Token.create(tokenvm);
     };
 
